cc9a8509a46a1340db1b5f0f2f37fac2a4f9658b
mfaseehdher <mfaseehdher@gmail.com>
2026-01-28 15:43:13 +0100

    Add files via upload

---
+++ cruise_control_fmu/plot.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+files = [
+    "result_m1000_b50_u500.csv",
+    "result_m2000_b50_u500.csv",
+    "result_m1000_b100_u500.csv",
+]
+
+plt.figure()
+
+for file in files:
+    df = pd.read_csv(file)
+
+    # Extract parameters from the file itself
+    m = int(df["m"].iloc[0])
+    b = int(df["b"].iloc[0])
+
+    plt.plot(df["time"], df["Out1"], label=f"m={m}, b={b}")
+
+plt.xlabel("Time (s)")
+plt.ylabel("V")
+plt.title("Parameters comparision")
+plt.grid(True)
+plt.legend()
+plt.savefig("combined_parameter_plot.png", dpi=300)
+print("Plot saved as combined_parameter_plot.png")
+
